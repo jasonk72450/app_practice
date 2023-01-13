752196eca660cff0c90929f33dce3ff5741a1b6a
jasonk72450 <totalhometour@gmail.com>
2023-01-13 16:55:42 -0600

    added basic 1

---
+++ basic_1.py
@@ -0,0 +1,10 @@
+# Basic 1
+# Prints a string in a specific format
+
+print("""
+Twinkle, twinkle, little star,
+    How I wonder what you are! 
+        Up above the world so high, 
+            Like a diamond in the sky.
+Twinkle, twinkle little star,
+    How I wonder what you are!""")
